4deedbae87937c279c7ecb091369edec68929181
bboyho <ho.chan@colorado.edu>
2017-04-03 19:37:50 -0600

    Update FPS_Blink.ino

--- FPS_GT511C3/Examples/FPS_Blink/FPS_Blink.ino
+++ FPS_GT511C3/Examples/FPS_Blink/FPS_Blink.ino
@@ -19,24 +19,29 @@
 It is recommended to use a dedicated bi-direcitonal LLC
 [ https://www.sparkfun.com/products/12009 ] for a reliable connection if you
 are using a 5V Arduino microcontroller:
+
    Fingerprint Scanner (Pin #) <-> Logic Level Converter <-> 5V Arduino w/ Atmega328P
      UART_TX (3.3V TTL)(Pin 1) <->     LV4 <-> HV4       <->  RX (pin 4)
      UART_RX (3.3V TTL)(Pin 2) <->     LV1 <-> HV1       <->  TX (pin 5)
            GND         (Pin 3) <->     GND <-> GND       <->     GND
       Vin (3.3V~6V)    (Pin 4) <->        HV             <->      5V
                                           LV             <->     3.3V
+					  
 2.) Voltage Division w/ 3x 10kOhm Resistors
 
 Otherwise, you could use 3x 10kOhm resistors [ https://www.sparkfun.com/products/11508 ]
 to divide the voltage from a 5V Arduino down to 3.3V FPS similar to the
 "Uni-Directional" application circuit on our old logic level converter
 [ https://cdn.sparkfun.com/assets/b/0/e/1/0/522637c6757b7f2b228b4568.png ]:
+
     Voltage Divider         <-> Fingerprint Scanner(Pin #) <-> Voltage Divider <-> 5V Arduino w/ Atmega328P
                             <-> UART_TX (3.3V TTL) (Pin 1) <->                 <->       RX (pin 4)
   GND <-> 10kOhm <-> 10kOhm <-> UART_RX (3.3V TTL) (Pin 2) <->      10kOhm     <->       TX (pin 5)
           GND               <->        GND         (Pin 3) <->       GND       <->        GND
                             <->    Vin (3.3V~6V)   (Pin 4) <->                 <->        5V
+			    
 Note: You can add the two 10kOhm resistors in series for 20kOhms. =)
+
 --------------------------------------------------------------------------------*/
 
 // set up software serial pins for Arduino's w/ Atmega328P's
